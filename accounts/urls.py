04ead5c6309ace209374fffe4096b1630bf3a7c8
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-06-10 23:12:19 +0330

    login account created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from .views import register_view
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
+
     path(
         "register/",
         register_view,
         name="register"
+    ),
+
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="accounts/login.html"
+        ),
+        name="login"
     ),
 ]
